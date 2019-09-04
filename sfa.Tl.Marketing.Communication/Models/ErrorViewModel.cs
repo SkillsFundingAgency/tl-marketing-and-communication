7f4832611f86302d7d858db7a5e3b613bc81baa0
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-09-04 14:47:11 +0100

    Add Standard Asp.net core site

---
+++ sfa.Tl.Marketing.Communication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sfa.Tl.Marketing.Communication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
