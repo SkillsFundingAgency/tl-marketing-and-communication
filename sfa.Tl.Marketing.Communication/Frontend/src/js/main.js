0e53e92157b861a5e9ce6cfb456c4b04658c037d
Ashley Anderton <ashley.j.anderton@gmail.com>
2019-09-09 15:02:01 +0100

    Multiple frontend fixes

--- sfa.Tl.Marketing.Communication/Frontend/src/js/main.js
+++ sfa.Tl.Marketing.Communication/Frontend/src/js/main.js
@@ -15,12 +15,32 @@ $(".tl-nav--hamburger").click(function () {
 
 
 
-$(".tl-link--modal").click(function () {
-    event.preventDefault();
-    $(this).next('.tl-modal').addClass('active');
-});
+
 
 $(".tl-modal--close").click(function () {
     event.preventDefault();
     $(this).closest('.tl-modal').removeClass('active');
+    $("body").removeClass('modal-open');
+
+});
+
+
+$(".tl-modal--content").click(function (e) {
+    e.stopPropagation();
+});
+
+$(".tl-link--modal").click(function () {
+    event.preventDefault();
+    $(this).next('.tl-modal').addClass('active');
+    $("body").addClass('modal-open');
+    event.stopImmediatePropagation()
+});
+
+
+$(document).on('click', function () {
+    if ($("body").hasClass("modal-open")) {
+        event.preventDefault();
+        $('.tl-modal').removeClass('active');
+        $("body").removeClass('modal-open');
+    }
 });
