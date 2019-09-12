eebcf00b00305af2a61a553d07cfd2ddfae50d38
Hardik Desai <hardik.desai@digital.education.gov.uk>
2019-09-12 14:54:09 +0100

    Default task does not need to watch for file change

--- sfa.Tl.Marketing.Communication/gulpfile.js
+++ sfa.Tl.Marketing.Communication/gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp');
 require('./gulp/tasks/default');
 require('./gulp/tasks/dev');
 
-gulp.task('default', ['bootstrap', 'assets', 'sass', 'js', 'sass:watch', 'js:watch', 'json' ]);
+gulp.task('default', ['bootstrap', 'assets', 'sass', 'js', 'json' ]);
 
 
 gulp.task('dev', ['bootstrap', 'assets', 'sass', 'dev.js', 'sass:watch', 'dev.js:watch', 'json']);
