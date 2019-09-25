d421c5340402edb215402700a1246b6a315a3d9b
Ashley Anderton <ashley.j.anderton@gmail.com>
2019-09-25 13:39:50 +0100

    Various content, footer, pages, fixes etc

---
+++ sfa.Tl.Marketing.Communication/Controllers/HelpController.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using sfa.Tl.Marketing.Communication.Models;
+
+namespace sfa.Tl.Marketing.Communication.Controllers
+{
+    public class HelpController : Controller
+    {
+
+        [Route("cookies", Name = "Cookies")]
+        public IActionResult Cookies()
+        {
+            return View();
+        }
+
+        [Route("accessibility", Name = "Accessibility")]
+        public IActionResult Accessibility()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
