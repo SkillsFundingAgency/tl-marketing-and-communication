c950f76f9b5c5a3163ece59ce64ec9f56b78d075
Adam <adamesfa@outlook.com>
2019-11-08 10:11:05 +0000

    Layer focus - modal
    Fix tabs in the modal on the Subjects page
    Allow the escape to be pressed to the close the modal

--- sfa.Tl.Marketing.Communication/Frontend/src/js/main.js
+++ sfa.Tl.Marketing.Communication/Frontend/src/js/main.js
@@ -12,23 +12,61 @@ $(".tl-nav--hamburger").click(function () {
     }
 });
 
+var firstTabStop;
+var lastTabStop;
+
 $(document).on('click', function () {
-    var target = event.target;
-    var parent = target.parentElement;
-    var modalcontent = $(".tl-modal--content")
+    var target = event.target
+    var modalContent = $(".tl-modal--content");
 
     if ($(target).is(".tl-link--modal")) {
         event.preventDefault();
         $(target).next(".tl-modal").addClass('active');
+        $(target).next(".tl-modal")[0].addEventListener('keydown', processKeyboardEvents);
         $("body").addClass('modal-open');
-        event.stopImmediatePropagation()
+        event.stopImmediatePropagation();
+
+        const elementsThatAreFocusable = $('.tl-modal.active a[href], area[href], select:not([disabled]), textarea:not([disabled]), button:not([disabled]), [tabindex="0"]');
+
+        firstTabStop = elementsThatAreFocusable[0];
+        lastTabStop = elementsThatAreFocusable[elementsThatAreFocusable.length - 1];
+
+        firstTabStop.focus();
     }
-    else if ($("body").hasClass("modal-open") && !$(target).is(modalcontent) && !modalcontent.has(target).length > 0 || $(target).is(".tl-modal--close")) {
+    else if ($("body").hasClass("modal-open") && !$(target).is(modalContent) && !modalContent.has(target).length > 0 || $(target).is(".tl-modal--close")) {
         event.preventDefault();
-        $('.tl-modal').removeClass('active');
-        $("body").removeClass('modal-open');
+        closeModal();
     }
 });
+
+function closeModal() {
+    $(".tl-modal").removeClass("active");
+    $("body").removeClass("modal-open");
+}
+
+function processKeyboardEvents(e) {
+    const keyTab = 9;
+    const keyEscape = 27;
+
+    if (e.keyCode === keyTab) {
+
+        if (e.shiftKey) {
+            if (document.activeElement === firstTabStop) {
+                e.preventDefault();
+                lastTabStop.focus();
+            }
+        } else {
+            if (document.activeElement === lastTabStop) {
+                e.preventDefault();
+                firstTabStop.focus();
+            }
+        }
+    }
+
+    if (e.keyCode === keyEscape) {
+        closeModal();
+    }
+}
 
 var maps = (function () {
     function initMap() {
@@ -47,7 +85,7 @@ var maps = (function () {
                         dropdown.val(key);
                     }
                 });
-            
+
             var map = new google.maps.Map(document.getElementById("map"),
                 {
                     center: { lat: 52.4774169, lng: -1.9336707 },
