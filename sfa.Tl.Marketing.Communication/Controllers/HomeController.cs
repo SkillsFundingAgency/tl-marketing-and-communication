ef017f388c614d48b1d0baa91cb808201cff3310
Ashley Anderton <ashley.j.anderton@gmail.com>
2020-02-04 12:07:51 +0000

    Fixed routing

--- sfa.Tl.Marketing.Communication/Controllers/HomeController.cs
+++ sfa.Tl.Marketing.Communication/Controllers/HomeController.cs
@@ -1,71 +1,14 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using sfa.Tl.Marketing.Communication.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace sfa.Tl.Marketing.Communication.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ConfigurationOptions _configuration;
-
-        public HomeController(ConfigurationOptions configuration)
-        {
-            _configuration = configuration;
-        }
-
-        [Route("/", Name = "Index")]
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [Route("/Home", Name = "Home")]
-        public IActionResult Home()
-        {
-            return RedirectToAction(nameof(Index));
-        }
-
-        [Route("about", Name = "About")]
-        public IActionResult About()
-        {
-            return View();
-        }
-
-        [Route("why", Name = "Why")]
-        public IActionResult Why()
-        {
-            return View();
-        }
-
-        [Route("subjects", Name = "Subjects")]
-        public IActionResult Subjects()
-        {
-            return View();
-        }
-
-
-        [Route("landing", Name = "Landing")]
+        [Route("/", Name = "Landing")]
         public IActionResult Landing()
         {
             return View();
         }
 
-
-        [Route("find", Name = "Find")]
-        public IActionResult Find(FindViewModel viewModel)
-        {
-            if (!string.IsNullOrEmpty(viewModel.Postcode))
-                viewModel.ShouldSearch = true;
-
-            viewModel.MapApiKey = _configuration.GoogleMapsApiKey;
-
-            return View(viewModel);
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
---
+++ sfa.Tl.Marketing.Communication/Controllers/StudentController.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using sfa.Tl.Marketing.Communication.Models;
+
+namespace sfa.Tl.Marketing.Communication.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ConfigurationOptions _configuration;
+
+        public StudentController(ConfigurationOptions configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Route("/student", Name = "Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/student/about", Name = "About")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("/student/why", Name = "Why")]
+        public IActionResult Why()
+        {
+            return View();
+        }
+
+        [Route("/student/subjects", Name = "Subjects")]
+        public IActionResult Subjects()
+        {
+            return View();
+        }
+
+
+        [Route("/student/find", Name = "Find")]
+        public IActionResult Find(FindViewModel viewModel)
+        {
+            if (!string.IsNullOrEmpty(viewModel.Postcode))
+                viewModel.ShouldSearch = true;
+
+            viewModel.MapApiKey = _configuration.GoogleMapsApiKey;
+
+            return View(viewModel);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
