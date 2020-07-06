3980be44d2a395ab657dcc0843a9e50ea67269e6
Mike Wild <wildconsultingltd@googlemail.com>
2020-07-06 11:37:05 +0100

    code tidying

--- sfa.Tl.Marketing.Communication.DataLoad/PostcodesIo/PostCodeLookupResultDto.cs
+++ sfa.Tl.Marketing.Communication.DataLoad/PostcodesIo/PostCodeLookupResultDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace sfa.Tl.Marketing.Communication.DataLoad.PostcodesIo
 {
-    public class PostCodeLookupResultDto
+    public class PostcodeLookupResultDto
     {
         public string Postcode { get; set; }
         public string Longitude { get; set; }
