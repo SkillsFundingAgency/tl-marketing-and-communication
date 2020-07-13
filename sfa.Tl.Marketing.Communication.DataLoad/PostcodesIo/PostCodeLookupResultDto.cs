d4eac64eccf4b3ccff2468af6438f3c5c046abb0
Mike Wild <wildconsultingltd@googlemail.com>
2020-07-13 14:03:05 +0100

    Remove unused properties in postcodes.io results

--- sfa.Tl.Marketing.Communication.DataLoad/PostcodesIo/PostCodeLookupResultDto.cs
+++ sfa.Tl.Marketing.Communication.DataLoad/PostcodesIo/PostCodeLookupResultDto.cs
@@ -5,11 +5,5 @@
         public string Postcode { get; set; }
         public string Longitude { get; set; }
         public string Latitude { get; set; }
-        public string Country { get; set; }
-        public string Region { get; set; }
-        public string OutCode { get; set; }
-        public string Admin_District { get; set; }
-        public string Admin_County { get; set; }
-        public LocationCodesDto Codes { get; set; }
     }
 }
