cb5247d95da8908cacdaca4316169fb5f4ba3329
Mike Wild <wildconsultingltd@googlemail.com>
2020-12-10 14:28:03 +0000

    Remove unused fields from PostcodeLookupResultDto

--- sfa.Tl.Marketing.Communication.Models/Dto/PostcodeLookupResultDto.cs
+++ sfa.Tl.Marketing.Communication.Models/Dto/PostcodeLookupResultDto.cs
@@ -11,23 +11,5 @@ namespace sfa.Tl.Marketing.Communication.Models.Dto
 
         [JsonPropertyName("latitude")]
         public string Latitude { get; set; }
-
-        [JsonPropertyName("country")]
-        public string Country { get; set; }
-
-        [JsonPropertyName("region")]
-        public string Region { get; set; }
-
-        [JsonPropertyName("outcode")]
-        public string Outcode { get; set; }
-
-        [JsonPropertyName("admin_district")]
-        public string AdminDistrict { get; set; }
-
-        [JsonPropertyName("admin_county")]
-        public string AdminCounty { get; set; }
-
-        [JsonPropertyName("codes")]
-        public LocationCodesDto Codes { get; set; }
     }
 }
