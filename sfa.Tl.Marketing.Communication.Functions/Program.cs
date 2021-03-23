55aa50d0291b33917c4e0e4dd6f93edbcef73ce1
Mike Wild <wildconsultingltd@googlemail.com>
2021-03-23 14:56:45 +0000

    Tidy functions Program startup

--- sfa.Tl.Marketing.Communication.Functions/Program.cs
+++ sfa.Tl.Marketing.Communication.Functions/Program.cs
@@ -23,63 +23,45 @@ namespace sfa.Tl.Marketing.Communication.Functions
                 .ConfigureFunctionsWorkerDefaults()
                 .ConfigureAppConfiguration(c =>
                     {
-                        Console.WriteLine("Loading configuration ...");
                         c.AddCommandLine(args);
                         c.AddJsonFile("local.settings.json", optional: true, reloadOnChange: false);
                         c.AddJsonFile("local.settings.development.json", optional: true, reloadOnChange: false);
                         c.AddEnvironmentVariables();
-                        Console.WriteLine("... Loaded configuration");
                     }
                 )
-                //.ConfigureServices(s =>
                 .ConfigureServices((hostContext, services) =>
                 {
-                    Console.WriteLine("Configuring services ...");
-
                     var config = hostContext.Configuration;
+                    var (apiConfig, storageConfig) =
+                    (
+                        new CourseDirectoryApiSettings
+                        {
+                            ApiKey = config.GetConfigurationValue(ConfigurationKeys.CourseDirectoryApiKeyConfigKey),
+                            ApiBaseUri = config.GetConfigurationValue(ConfigurationKeys.CourseDirectoryApiBaseUriConfigKey)
+                        },
+                        new StorageSettings
+                        {
+                            TableStorageConnectionString = config.GetConfigurationValue(ConfigurationKeys.TableStorageConnectionStringConfigKey)
+                        }
+                    );
 
-                    Console.WriteLine("... Loading configuration ...");
-
-                    LoadConfiguration(config);
-
-                    Console.WriteLine("... Registering http clients ...");
-                    RegisterHttpClients(services);
-
-                    Console.WriteLine("... Registering services ...");
-                    RegisterServices(services);
-
-                    Console.WriteLine("... Configured services");
+                    RegisterHttpClients(services, apiConfig);
+                    RegisterServices(services, storageConfig);
                 })
                 .Build();
 
             await host.RunAsync();
         }
 
-        private static void LoadConfiguration(IConfiguration config)
-        {
-            ApiConfiguration = new CourseDirectoryApiSettings
-            {
-                ApiKey = config.GetConfigurationValue(ConfigurationKeys.CourseDirectoryApiKeyConfigKey),
-                ApiBaseUri = config.GetConfigurationValue(ConfigurationKeys.CourseDirectoryApiBaseUriConfigKey)
-            };
-
-            Console.WriteLine($"   --> ApiConfiguration.ApiBaseUri {ApiConfiguration.ApiBaseUri}");
-            StorageConfiguration = new StorageSettings
-            {
-                TableStorageConnectionString = config.GetConfigurationValue(ConfigurationKeys.TableStorageConnectionStringConfigKey)
-            };
-            Console.WriteLine($"   --> StorageConfiguration.TableStorageConnectionString {StorageConfiguration.TableStorageConnectionString}");
-        }
-
-        private static void RegisterHttpClients(IServiceCollection services)
+        private static void RegisterHttpClients(IServiceCollection services, CourseDirectoryApiSettings apiConfiguration)
         {
             services.AddHttpClient<ICourseDirectoryDataService, CourseDirectoryDataService>(
                     CourseDirectoryDataService.CourseDirectoryHttpClientName,
                     client =>
                     {
-                        client.BaseAddress = new Uri(ApiConfiguration.ApiBaseUri);
+                        client.BaseAddress = new Uri(apiConfiguration.ApiBaseUri);
                         client.DefaultRequestHeaders.Add("Accept", "application/json");
-                        client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", ApiConfiguration.ApiKey);
+                        client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", apiConfiguration.ApiKey);
                         client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
                         client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("deflate"));
                     }
@@ -96,22 +78,15 @@ namespace sfa.Tl.Marketing.Communication.Functions
                 });
         }
 
-        protected static CourseDirectoryApiSettings ApiConfiguration;
-        protected static StorageSettings StorageConfiguration;
-
-        private static void RegisterServices(IServiceCollection services)
+        private static void RegisterServices(IServiceCollection services, StorageSettings storageConfiguration)
         {
-            services.AddSingleton(ApiConfiguration);
-            services.AddSingleton(StorageConfiguration);
-
             var cloudStorageAccount =
-                CloudStorageAccount.Parse(StorageConfiguration.TableStorageConnectionString);
+                CloudStorageAccount.Parse(storageConfiguration.TableStorageConnectionString);
             services.AddSingleton(cloudStorageAccount);
             var cloudTableClient = cloudStorageAccount.CreateCloudTableClient();
             services.AddSingleton(cloudTableClient);
 
             services.AddTransient(typeof(ICloudTableRepository<>), typeof(GenericCloudTableRepository<>));
-
             services.AddTransient<ICourseDirectoryDataService, CourseDirectoryDataService>();
             services.AddTransient<ITableStorageService, TableStorageService>();
         }
