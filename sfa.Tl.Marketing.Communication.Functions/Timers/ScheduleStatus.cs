a883f1b87272e6ee9f4bc1c50ab0befe3e8a97c3
Mike Wild <wildconsultingltd@googlemail.com>
2021-05-11 14:14:00 +0100

    Refactoring - split TimerInfo class

---
+++ sfa.Tl.Marketing.Communication.Functions/Timers/ScheduleStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// ReSharper disable ClassNeverInstantiated.Global
+
+namespace sfa.Tl.Marketing.Communication.Functions.Timers
+{
+    public class ScheduleStatus
+    {
+        public DateTime Last { get; set; }
+
+        public DateTime Next { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+    }
+}
