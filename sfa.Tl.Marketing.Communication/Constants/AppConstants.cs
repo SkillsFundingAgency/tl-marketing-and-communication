d214517c628019ce72e33c7b8cad52a0da775810
Mike Wild <wildconsultingltd@googlemail.com>
2021-07-28 11:05:30 +0100

    Remove EmployerContactViewModel
    No longer needed on Next Steps

--- sfa.Tl.Marketing.Communication/Constants/AppConstants.cs
+++ sfa.Tl.Marketing.Communication/Constants/AppConstants.cs
@@ -3,7 +3,6 @@
     public static class AppConstants
     {
         public const int DefaultNumberOfItemsToShow = 5;
-        public const string EmployerContactFormSentCookieName = "ContactFormSent";
         public const string PostcodeValidationMessage = "You must enter a postcode";
         public const string RealPostcodeValidationMessage = "You must enter a real postcode";
         public const string ValidationStyle = "tl-validation--error";
