cba3f0dec6a797a2cb896b28c3a8d5abfb6dba56
Mike Wild <wildconsultingltd@googlemail.com>
2021-09-09 15:01:24 +0100

    Add IsAvailableNow and DebuggerDisplay to models

--- sfa.Tl.Marketing.Communication.Models/Dto/DeliveryYear.cs
+++ sfa.Tl.Marketing.Communication.Models/Dto/DeliveryYear.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace sfa.Tl.Marketing.Communication.Models.Dto
 {
+    [DebuggerDisplay("{DebuggerDisplay(), nq}")]
     public class DeliveryYear
     {
         public short Year { get; init; }
 
-        public IEnumerable<Qualification> Qualifications { get; init; }
+        public bool IsAvailableNow { get; set; }
+
+        public IList<Qualification> Qualifications { get; set; }
+        
+        private string DebuggerDisplay()
+            => $"{Year} " +
+               $"{(IsAvailableNow ? "(Available now) " : "")}" +
+               $"{(Qualifications != null ? Qualifications.Count : "null")} Qualifications";
     }
 }
--- sfa.Tl.Marketing.Communication/Models/DeliveryYearViewModel.cs
+++ sfa.Tl.Marketing.Communication/Models/DeliveryYearViewModel.cs
@@ -1,10 +1,20 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace sfa.Tl.Marketing.Communication.Models
 {
+    [DebuggerDisplay("{DebuggerDisplay(), nq}")]
     public class DeliveryYearViewModel
     {
         public short Year { get; set; }
+        
+        public bool IsAvailableNow { get; set; }
+
         public IList<QualificationViewModel> Qualifications { get; set; }
+
+        private string DebuggerDisplay()
+            => $"{Year} " +
+               $"{(IsAvailableNow ? "(Available now) " : "")}" +
+               $"{(Qualifications != null ? Qualifications.Count : "null")} Qualifications";
     }
 }
