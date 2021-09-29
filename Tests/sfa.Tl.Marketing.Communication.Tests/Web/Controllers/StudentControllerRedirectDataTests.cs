2deba59e05b9823cc1fce780ac991fa462edd084
Mike Wild <wildconsultingltd@googlemail.com>
2021-09-29 10:52:46 +0100

    Add and update tests

--- Tests/sfa.Tl.Marketing.Communication.Tests/Web/Controllers/StudentControllerRedirectDataTests.cs
+++ Tests/sfa.Tl.Marketing.Communication.Tests/Web/Controllers/StudentControllerRedirectDataTests.cs
@@ -4,9 +4,8 @@ using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
 using sfa.Tl.Marketing.Communication.Application.Interfaces;
-using sfa.Tl.Marketing.Communication.Controllers;
 using sfa.Tl.Marketing.Communication.Models;
-using sfa.Tl.Marketing.Communication.SearchPipeline;
+using sfa.Tl.Marketing.Communication.UnitTests.Builders;
 using Xunit;
 
 namespace sfa.Tl.Marketing.Communication.UnitTests.Web.Controllers
@@ -22,8 +21,6 @@ namespace sfa.Tl.Marketing.Communication.UnitTests.Web.Controllers
         [InlineData("https://www.stokesfc.ac.uk/our-courses/?tex_post_tag=t-level&view=list")]
         public void Student_Controller_Redirect_Returns_Expected_Redirect_For_Known_Uri(string targetUri)
         {
-            var providerSearchEngine = Substitute.For<IProviderSearchEngine>();
-
             var providerDataService = Substitute.For<IProviderDataService>();
             providerDataService
                 .GetWebsiteUrls()
@@ -36,10 +33,7 @@ namespace sfa.Tl.Marketing.Communication.UnitTests.Web.Controllers
             urlHelper.IsLocalUrl(Arg.Any<string>())
                 .Returns(args => ((string)args[0]).StartsWith("/students/"));
 
-            var controller = new StudentController(providerDataService, providerSearchEngine)
-            {
-                Url = urlHelper
-            };
+            var controller = new StudentControllerBuilder().BuildStudentController(providerDataService, urlHelper: urlHelper);
 
             var redirectResult = controller.Redirect(
                 new RedirectViewModel
