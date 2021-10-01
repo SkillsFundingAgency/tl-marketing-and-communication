a8e48af48dedb130341a5f0e12a5b88a15d00195
Mike Wild <wildconsultingltd@googlemail.com>
2021-10-01 11:49:07 +0100

    Refactor tests

--- Tests/sfa.Tl.Marketing.Communication.Tests/Web/SearchPipeline/Steps/MergeAvailableDeliveryYearsStepTests.cs
+++ Tests/sfa.Tl.Marketing.Communication.Tests/Web/SearchPipeline/Steps/MergeAvailableDeliveryYearsStepTests.cs
@@ -5,9 +5,9 @@ using sfa.Tl.Marketing.Communication.Application.Interfaces;
 using sfa.Tl.Marketing.Communication.Models;
 using sfa.Tl.Marketing.Communication.SearchPipeline;
 using sfa.Tl.Marketing.Communication.SearchPipeline.Steps;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using sfa.Tl.Marketing.Communication.UnitTests.Builders;
 using Xunit;
 
 namespace sfa.Tl.Marketing.Communication.UnitTests.Web.SearchPipeline.Steps
@@ -20,7 +20,9 @@ namespace sfa.Tl.Marketing.Communication.UnitTests.Web.SearchPipeline.Steps
             var dateTimeService = Substitute.For<IDateTimeService>();
             dateTimeService.Today.Returns(DateTime.Parse("2021-08-31"));
 
-            var providerLocations = BuildProviderLocationViewModelList();
+            var providerLocations = new ProviderLocationViewModelListBuilder()
+                .Add()
+                .Build();
 
             var viewModel = new FindViewModel
             {
@@ -44,7 +46,9 @@ namespace sfa.Tl.Marketing.Communication.UnitTests.Web.SearchPipeline.Steps
             var dateTimeService = Substitute.For<IDateTimeService>();
             dateTimeService.Today.Returns(DateTime.Parse("2021-09-01"));
 
-            var providerLocations = BuildProviderLocationViewModelList();
+            var providerLocations = new ProviderLocationViewModelListBuilder()
+                .Add()
+                .Build();
 
             var viewModel = new FindViewModel
             {
@@ -67,38 +71,5 @@ namespace sfa.Tl.Marketing.Communication.UnitTests.Web.SearchPipeline.Steps
             qualifications[1].Id.Should().Be(2);
             qualifications[1].Name.Should().Be("Test Qualification 2");
         }
-
-        public IList<ProviderLocationViewModel> BuildProviderLocationViewModelList() =>
-            new List<ProviderLocationViewModel>
-            {
-                new()
-                {
-                    ProviderName = "Test Provider",
-                    Name = "Test Provider",
-                    Postcode = "CV1 2WT",
-                    DeliveryYears = BuildDeliveryYearViewModelList()
-                }
-            };
-
-        public IList<DeliveryYearViewModel> BuildDeliveryYearViewModelList() =>
-            new List<DeliveryYearViewModel>
-            {
-                new()
-                {
-                    Year = 2021,
-                    Qualifications = new List<QualificationViewModel>
-                    {
-                        new() { Id = 1, Name = "Test Qualification 1" }
-                    }
-                },
-                new()
-                {
-                    Year = 2021,
-                    Qualifications = new List<QualificationViewModel>
-                    {
-                        new() { Id = 2, Name = "Test Qualification 2" }
-                    }
-                }
-            };
     }
 }
