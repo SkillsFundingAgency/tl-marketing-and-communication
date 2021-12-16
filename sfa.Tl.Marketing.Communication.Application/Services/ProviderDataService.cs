c1c13b94899e84b4d3001dcc9a4d968b5414092e
Mike Wild <wildconsultingltd@googlemail.com>
2021-12-16 16:34:01 +0000

    Initial changes to load temporary provider data

--- sfa.Tl.Marketing.Communication.Application/Services/ProviderDataService.cs
+++ sfa.Tl.Marketing.Communication.Application/Services/ProviderDataService.cs
@@ -4,7 +4,11 @@ using sfa.Tl.Marketing.Communication.Models.Dto;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
 using Microsoft.Extensions.Caching.Memory;
+using sfa.Tl.Marketing.Communication.Application.Extensions;
 using sfa.Tl.Marketing.Communication.Models.Configuration;
 
 namespace sfa.Tl.Marketing.Communication.Application.Services
@@ -14,9 +18,17 @@ namespace sfa.Tl.Marketing.Communication.Application.Services
         private const string ProviderTableDataCacheKey = "Provider_Table_Data";
         private const string QualificationTableDataCacheKey = "Qualification_Table_Data";
         private readonly int _cacheExpiryInSeconds;
+        private readonly bool _mergeTempProviderData;
 
         private readonly IMemoryCache _cache;
         private readonly ITableStorageService _tableStorageService;
+
+        private static readonly IDictionary<long, Provider> TempProviderData;
+
+        static ProviderDataService()
+        {
+            TempProviderData = LoadTempProviderData();
+        }
 
         public ProviderDataService(
             ITableStorageService tableStorageService,
@@ -26,12 +38,13 @@ namespace sfa.Tl.Marketing.Communication.Application.Services
             _tableStorageService = tableStorageService ?? throw new ArgumentNullException(nameof(tableStorageService));
             _cache = cache ?? throw new ArgumentNullException(nameof(cache));
             _cacheExpiryInSeconds = configuration?.CacheExpiryInSeconds ?? 60;
+            _mergeTempProviderData = configuration?.MergeTempProviderData ?? false;
         }
-        
+
         public IQueryable<ProviderLocation> GetProviderLocations(int? qualificationId = null)
         {
             var providerLocations = new List<ProviderLocation>();
-            
+
             var qualificationsDictionary = GetAllQualifications()
                 .ToDictionary(q => q.Id);
 
@@ -72,7 +85,7 @@ namespace sfa.Tl.Marketing.Communication.Application.Services
 
             return providerLocations.AsQueryable();
         }
-        
+
         public IEnumerable<Qualification> GetQualifications(int[] qualificationIds)
         {
             var qualifications = GetAllQualifications();
@@ -131,7 +144,7 @@ namespace sfa.Tl.Marketing.Communication.Application.Services
             return qualifications;
         }
 
-        private IList<Qualification> GetQualificationsForDeliveryYear(
+        private static IList<Qualification> GetQualificationsForDeliveryYear(
             DeliveryYearDto deliveryYear,
             IDictionary<int, Qualification> qualificationsDictionary)
         {
@@ -176,6 +189,50 @@ namespace sfa.Tl.Marketing.Communication.Application.Services
             if (_cacheExpiryInSeconds > 0)
                 options.SetAbsoluteExpiration(TimeSpan.FromSeconds(_cacheExpiryInSeconds));
             return options;
+        }
+
+        private static IDictionary<long, Provider> LoadTempProviderData()
+        {
+            var assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+            var jsonFile = $"{assemblyName}.Data.ProviderData.json";
+
+            return JsonDocument
+                .Parse(jsonFile.ReadManifestResourceStreamAsString())
+                .RootElement
+                .GetProperty("providers")
+                .EnumerateArray()
+                .Select(p =>
+                    new Provider
+                    {
+                        UkPrn = p.GetProperty("ukPrn").GetInt64(),
+                        Name = p.GetProperty("name").GetString(),
+                        Locations = p.GetProperty("locations")
+                            .EnumerateArray()
+                            .Select(l =>
+                                new Location
+                                {
+                                    Postcode = l.GetProperty("postcode").GetString(),
+                                    Name = l.SafeGetString("name"),
+                                    Town = l.SafeGetString("town"),
+                                    Latitude = l.SafeGetDouble("latitude"),
+                                    Longitude = l.SafeGetDouble("longitude"),
+                                    Website = l.SafeGetString("website"),
+                                    DeliveryYears = l.TryGetProperty("deliveryYears", out var deliveryYears)
+                                        ? deliveryYears.EnumerateArray()
+                                            .Select(d =>
+                                                new DeliveryYearDto
+                                                {
+                                                    Year = d.GetProperty("year").GetInt16(),
+                                                    Qualifications = d.GetProperty("qualifications")
+                                                        .EnumerateArray()
+                                                        .Select(q => q.GetInt32())
+                                                        .ToList()
+                                                })
+                                            .ToList()
+                                        : new List<DeliveryYearDto>()
+                                }).ToList()
+                    })
+                .ToDictionary(p => p.UkPrn);
         }
     }
 }
