b226d30963983519a87582a3c44ea8684e3082f2
Mike Wild <wildconsultingltd@googlemail.com>
2022-01-27 14:01:42 +0000

    Fis apostrophe issues in ToTitleCase

--- sfa.Tl.Marketing.Communication.Models/Extensions/StringExtensions.cs
+++ sfa.Tl.Marketing.Communication.Models/Extensions/StringExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace sfa.Tl.Marketing.Communication.Models.Extensions;
 
@@ -9,7 +10,7 @@ public static class StringExtensions
 {
     public static string ToLetterOrDigit(this string value)
     {
-        return string.IsNullOrWhiteSpace(value) ? string.Empty : 
+        return string.IsNullOrWhiteSpace(value) ? string.Empty :
             new string(Array.FindAll(value.ToCharArray(), char.IsLetterOrDigit));
     }
 
@@ -29,18 +30,21 @@ public static class StringExtensions
 
         var artsAndPreps = new List<string>
         {
-            "a", "an", "and", "any", "at", "for", "from", "in", "into", 
+            "a", "an", "and", "any", "at", "for", "from", "in", "into",
             "of", "on", "or", "some", "the", "to"
         };
 
         result = tokens[0];
         tokens.RemoveAt(0);
-            
+
         result += tokens.Aggregate(string.Empty, (prev, input)
             => prev +
-               (artsAndPreps.Contains(input.ToLower()) 
+               (artsAndPreps.Contains(input.ToLower())
                    ? " " + input.ToLower() // Return the prep/art lowercase
                    : " " + input));        // Otherwise return the valid word
+
+        //Fix S after apostrophe, if it is before space or at end of string
+        result = Regex.Replace(result, @"(['’])S(\s|$)", "$1s$2");
 
         return result.Trim();
     }
