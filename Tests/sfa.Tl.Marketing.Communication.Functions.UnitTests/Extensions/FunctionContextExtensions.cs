d7b6679e4d508a60f1b038f1a5353cd73ba09e72
Mike Wild <wildconsultingltd@googlemail.com>
2022-06-08 10:23:31 +0100

    Code tidying

--- Tests/sfa.Tl.Marketing.Communication.Functions.UnitTests/Extensions/FunctionContextExtensions.cs
+++ Tests/sfa.Tl.Marketing.Communication.Functions.UnitTests/Extensions/FunctionContextExtensions.cs
@@ -6,34 +6,34 @@ using NSubstitute;
 
 namespace sfa.Tl.Marketing.Communication.Functions.UnitTests.Extensions;
 
-    public static class FunctionContextExtensions
+public static class FunctionContextExtensions
+{
+    public static HttpRequestData BuildHttpRequestData(
+        this FunctionContext functionContext, HttpMethod method = default)
     {
-        public static HttpRequestData BuildHttpRequestData(
-            this FunctionContext functionContext, HttpMethod method = default)
-        {
-            var request = Substitute.For<HttpRequestData>(functionContext);
-            request.Method
-                .Returns((method ?? HttpMethod.Get).ToString());
+        var request = Substitute.For<HttpRequestData>(functionContext);
+        request.Method
+            .Returns((method ?? HttpMethod.Get).ToString());
 
-            var responseData = functionContext.BuildHttpResponseData();
-            request.CreateResponse()
-                .Returns(responseData);
+        var responseData = functionContext.BuildHttpResponseData();
+        request.CreateResponse()
+            .Returns(responseData);
 
         return request;
-        }
+    }
 
-        public static HttpResponseData BuildHttpResponseData(
-            this FunctionContext functionContext)
-        {
-            var responseData = Substitute.For<HttpResponseData>(functionContext);
+    public static HttpResponseData BuildHttpResponseData(
+        this FunctionContext functionContext)
+    {
+        var responseData = Substitute.For<HttpResponseData>(functionContext);
 
-            var responseHeaders = new HttpHeadersCollection();
-            responseData.Headers.Returns(responseHeaders);
+        var responseHeaders = new HttpHeadersCollection();
+        responseData.Headers.Returns(responseHeaders);
 
-            var responseBody = new MemoryStream();
-            responseData.Body.Returns(responseBody);
+        var responseBody = new MemoryStream();
+        responseData.Body.Returns(responseBody);
 
-            return responseData;
-        }
+        return responseData;
+    }
 }
 
