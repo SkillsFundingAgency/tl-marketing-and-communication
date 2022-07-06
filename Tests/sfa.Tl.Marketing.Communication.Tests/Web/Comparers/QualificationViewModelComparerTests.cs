81586f517d2c7515f5743f3d05d266d690a0fbfb
Mike Wild <wildconsultingltd@googlemail.com>
2022-07-06 11:00:54 +0100

    TLWP-1725 - Add comparer so duplicate qualifications will be merged

---
+++ Tests/sfa.Tl.Marketing.Communication.Tests/Web/Comparers/QualificationViewModelComparerTests.cs
@@ -0,0 +1,100 @@
+﻿using FluentAssertions;
+using sfa.Tl.Marketing.Communication.Comparers;
+using sfa.Tl.Marketing.Communication.Models;
+using Xunit;
+
+namespace sfa.Tl.Marketing.Communication.UnitTests.Application.Comparers
+{
+    public class QualificationViewModelComparerTests
+    {
+        [Fact]
+        public void QualificationViewModelComparerTests_Returns_True_For_Same_Values()
+        {
+            var q1 = new QualificationViewModel
+            {
+                Id = 1,
+                Name = "Test 1"
+            };
+
+            var q2 = new QualificationViewModel
+            {
+                Id = 1,
+                Name = "Test 1"
+            };
+
+            var comparer = new QualificationViewModelComparer();
+
+            comparer.Equals(q1, q2).Should().BeTrue();
+        }
+
+        [Fact]
+        public void QualificationViewModelComparerTests_Returns_True_For_Same_Object()
+        {
+            var q1 = new QualificationViewModel
+            {
+                Id = 1,
+                Name = "Test 1"
+            };
+
+            var comparer = new QualificationViewModelComparer();
+
+            comparer.Equals(q1, q1).Should().BeTrue();
+        }
+
+        [Fact]
+        public void QualificationViewModelComparerTests_Returns_False_For_Different_Names()
+        {
+            var q1 = new QualificationViewModel
+            {
+                Id = 1,
+                Name = "Test 1"
+            };
+
+            var q2 = new QualificationViewModel
+            {
+                Id = 1,
+                Name = "Test 2"
+            };
+
+            var comparer = new QualificationViewModelComparer();
+
+            comparer.Equals(q1, q2).Should().BeTrue();
+        }
+        [Fact]
+        public void QualificationViewModelComparerTests_Returns_Not_Equals_For_Q1_Null()
+        {
+            var q2 = new QualificationViewModel
+            {
+                Id = 1,
+                Name = "Test 1"
+            };
+
+            var comparer = new QualificationViewModelComparer();
+
+            comparer.Equals(null, q2).Should().BeFalse();
+        }
+
+        [Fact]
+        public void QualificationViewModelComparerTests_Returns_Not_Equals_For_Q2_Null()
+        {
+            var q1 = new QualificationViewModel
+            {
+                Id = 1,
+                Name = "Test 1"
+            };
+            
+            var comparer = new QualificationViewModelComparer();
+
+            comparer.Equals(q1, null).Should().BeFalse();
+        }
+
+        [Fact]
+        public void QualificationViewModelComparerTests_Returns_Not_Equal_For_Both_Null()
+        {
+            var comparer = new QualificationViewModelComparer();
+
+            comparer.Equals(null, null).Should().BeFalse();
+        }
+
+    }
+}
---
+++ sfa.Tl.Marketing.Communication/Comparers/QualificationViewModelComparer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using sfa.Tl.Marketing.Communication.Models;
+
+namespace sfa.Tl.Marketing.Communication.Comparers;
+
+public class QualificationViewModelComparer : IEqualityComparer<QualificationViewModel>
+{
+    public bool Equals(QualificationViewModel q1, QualificationViewModel q2)
+    {
+        if (ReferenceEquals(q1, null)) return false;
+        if (ReferenceEquals(q2, null)) return false;
+        if (ReferenceEquals(q1, q2)) return true;
+        if (q1.GetType() != q2.GetType()) return false;
+
+        return q1.Id == q2.Id;
+    }
+
+    public int GetHashCode(QualificationViewModel q)
+    {
+        return q.Id;
+    }
+}
