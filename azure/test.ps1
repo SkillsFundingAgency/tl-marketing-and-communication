88d48217647973871d133e3c3efe9de59d3e06bc
Alan Mullett <AlanMullett@users.noreply.github.com>
2022-12-22 08:06:48 +0000

    add some extra output to report current activity

--- azure/test.ps1
+++ azure/test.ps1
@@ -123,8 +123,7 @@ if ($envDeployment.ProvisioningState -eq "Succeeded") {
     Write-Output "Yippee!!"
 }
 
-<# A section to allow easy cleanup of the environments, the first line is because I've been looking at migration from app insights.
-
+<#
 # you have to remove the diagnostic settings separately as they hang around if you don't and mess things up badly
 $subscriptionId = (Get-AzContext).Subscription.Id
 $diagnosticResourceIds = @(
@@ -140,9 +139,12 @@ foreach ($diagnosticResourceId in $diagnosticResourceIds) {
     }   
 }
 
+Write-Host "Removing OperationalInsights Workspace"
 Remove-AzOperationalInsightsWorkspace -ResourceGroupName $sharedResourceGroupName -Name "$($sharedResourceGroupName)-log" -ForceDelete -Force -ErrorAction SilentlyContinue
 
+Write-Host "Removing $($envResourceGroupName)"
 Remove-AzResourceGroup -ResourceGroupName $envResourceGroupName -Force -ErrorAction SilentlyContinue
+Write-Host "Removing $($sharedResourceGroupName)"
 Remove-AzResourceGroup -ResourceGroupName $sharedResourceGroupName -Force -ErrorAction SilentlyContinue
 #>
 
